2e676dc62434264fbff26ba6080b42be78a3f337
SortelOne <Sortel1@yahoo.com>
2017-11-30 17:40:39 -0700

    Tic Tac Toe is done!
    Yay!

--- Nov. 28/src/Main.java
+++ Nov. 28/src/Main.java
@@ -6,36 +6,44 @@ public class Main {
     public static String xOrY;
 
     public static void takeInput(){
-        boolean win = false;
         int row;
         int col;
         boolean xCoord = false;
         boolean yCoord = false;
         boolean let = false;
         char xxOrY;
+        boolean board = false;
         xOrY = "";
-        Scanner scan = new Scanner(System.in);
 
-        do{
-            System.out.println("Please put in your x coordinate and press enter.");
-            row = scan.nextInt();
-            if (row != 1 && row != 2 && row !=3) {
-                System.out.println("That is not within the bounds, try again");
-                row = 0;
+        Scanner scan = new Scanner(System.in);
+        do {
+            do {
+                System.out.println("Please put in your x coordinate and press enter.");
+                col = scan.nextInt();
+                if (col != 1 && col != 2 && col != 3) {
+                    System.out.println("That is not within the bounds, try again");
+                    row = 0;
+                }
+                if (col == 1 || col == 2 || col == 3)
+                    yCoord = true;
+            } while (yCoord == false);
+            do {
+                System.out.println("Please put in your y coordinate and press enter.");
+                row = scan.nextInt();
+                if (row != 1 && row != 2 && row != 3) {
+                    System.out.println("That is not within the bounds, try again");
+                    row = 0;
+                }
+                if (row == 1 || row == 2 || row == 3)
+                    xCoord = true;
+            } while (xCoord == false);
+            if (ticBoard[row][col]!="{ }"){
+                System.out.println("That spot is already taken, try a new one.");
             }
-            if(row==1 || row==2 || row==3)
-                xCoord = true;
-        }while(xCoord==false);
-        do{
-            System.out.println("Please put in your y coordinate and press enter.");
-            col = scan.nextInt();
-            if (col != 1 && col != 2 && col !=3) {
-                System.out.println("That is not within the bounds, try again");
-                row = 0;
+            else if(ticBoard[row][col] == "{ }"){
+                board=true;
             }
-            if(col == 1 || col == 2 || col == 3)
-                yCoord = true;
-        }while(yCoord==false);
+        }while(board==false);
         do{
             System.out.println("Now put in either X or O (Whichever you are).");
             xOrY = scan.next();
@@ -59,8 +67,90 @@ public class Main {
         let = false;
     }
 
-    public static void winCondition(){
+    public static boolean winCondition(){
+        boolean winner=false;
 
+        //Diagonal top left bottom right X
+        if(ticBoard[1][1]=="{X}" && ticBoard[2][2]=="{X}" && ticBoard[3][3]=="{X}") {
+            winner=true;
+            System.out.println("X won!");
+        }
+        //Diagonal top left bottom right O
+        else if(ticBoard[1][1]=="{O}" && ticBoard[2][2]=="{O}" && ticBoard[3][3]=="{O}") {
+            winner=true;
+            System.out.println("O won!");
+        }
+        //Diagonal top right bottom left X
+        else if(ticBoard[1][3]=="{X}" && ticBoard[2][2]=="{X}" && ticBoard[3][1]=="{X}"){
+            winner=true;
+            System.out.println("X won!");
+        }
+        //Diagonal top right bottom left O
+        else if(ticBoard[1][3]=="{O}" && ticBoard[2][2]=="{O}" && ticBoard[3][1]=="{O}"){
+            winner=true;
+            System.out.println("O won!");
+        }
+        //Bottom X
+        else if(ticBoard[3][1] == "{X}" && ticBoard[3][2] == "{X}" && ticBoard[3][3] == "{X}"){
+            winner=true;
+            System.out.println("X won!");
+        }
+        //Bottom O
+        else if(ticBoard[3][1] == "{O}" && ticBoard[3][2] == "{O}" && ticBoard[3][3] == "{O}"){
+            winner=true;
+            System.out.println("O won!");
+        }
+        //Middle X
+        else if(ticBoard[2][1] == "{X}" && ticBoard[2][2] == "{X}" && ticBoard[2][3] == "{X}"){
+            winner=true;
+            System.out.println("X won!");
+        }
+        //Middle O
+        else if(ticBoard[2][1] == "{O}" && ticBoard[2][2] == "{O}" && ticBoard[2][3] == "{O}"){
+            winner=true;
+            System.out.println("O won!");
+        }
+        //Top X
+        else if(ticBoard[1][1] == "{X}" && ticBoard[1][2] == "{X}" && ticBoard[1][3] == "{X}"){
+            winner=true;
+            System.out.println("X won!");
+        }
+        //Top O
+        else if(ticBoard[1][1] == "{O}" && ticBoard[1][2] == "{O}" && ticBoard[1][3] == "{O}"){
+            winner=true;
+            System.out.println("O won!");
+        }
+        //Left X
+        else if(ticBoard[1][1] == "{X}" && ticBoard[2][1] == "{X}" && ticBoard[3][1] == "{X}"){
+            winner=true;
+            System.out.println("X won!");
+        }
+        //Left O
+        else if(ticBoard[1][1] == "{O}" && ticBoard[2][1] == "{O}" && ticBoard[3][1] == "{O}"){
+            winner=true;
+            System.out.println("O won!");
+        }
+        //Mid X
+        else if(ticBoard[1][2] == "{X}" && ticBoard[2][2] == "{X}" && ticBoard[3][2] == "{X}"){
+            winner=true;
+            System.out.println("X won!");
+        }
+        //Mid O
+        else if(ticBoard[1][2] == "{O}" && ticBoard[2][2] == "{O}" && ticBoard[3][2] == "{O}"){
+            winner=true;
+            System.out.println("O won!");
+        }
+        //Right X
+        else if(ticBoard[1][3] == "{X}" && ticBoard[2][3] == "{X}" && ticBoard[3][3] == "{X}"){
+            winner=true;
+            System.out.println("X won!");
+        }
+        //Right O
+        else if(ticBoard[1][3] == "{O}" && ticBoard[2][3] == "{O}" && ticBoard[3][3] == "{O}"){
+            winner=true;
+            System.out.println("O won!");
+        }
+        return winner;
     }
 
     public static void buildTable() {
@@ -87,6 +177,7 @@ public class Main {
         System.out.println("Welcome to Tic Tac Toe!");
         do{
             takeInput();
+            win = winCondition();
         }while(win==false);
 
     }
